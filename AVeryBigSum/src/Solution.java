3d79267ae4f50feda67b600aae9184ba2373f226
Luis Rivera <lriveraf.27@gmail.com>
2016-08-09 12:31:01 -0400

    Completed AVeryBigSum

---
+++ AVeryBigSum/src/Solution.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        long veryBigNum = 0;
+        for(int arr_i=0; arr_i < n; arr_i++){
+            veryBigNum += in.nextInt();
+        }
+
+        System.out.println(veryBigNum);
+    }
+}
